4b8f1a9c8276fc4fba5479a1bb95a3ab17514c92
MyFrondEndProjects <163176778+MyFrondEndProjects@users.noreply.github.com>
2024-04-21 16:43:20 +0300

    App.js güncellendi

--- app.js
+++ app.js
@@ -1,56 +1,54 @@
-const content=document.querySelector(".Content-div");
-const input=document.querySelector(".input-div");
-const buton=document.querySelector("#buton");
-const girisform=document.querySelector("#input");
-const ul=document.querySelector(".list");
-const temizle=document.querySelector(".temizle");
-let bildirim=document.querySelector(".notification");
-let info=document.querySelector(".info");
-let sayac=0;
-function ekle()
-{
+const content = document.querySelector(".Content-div");
+const input = document.querySelector(".input-div");
+const buton = document.querySelector("#buton");
+const girisform = document.querySelector("#input");
+const ul = document.querySelector(".list");
+const temizle = document.querySelector(".temizle");
+let bildirim = document.querySelector(".notification");
+let info = document.querySelector(".info");
+let sayac = 0;
+function ekle() {
 
-   if(girisform.value!="")
-   {
+   if (girisform.value != "") {
 
-   sayac+=1;
-   let sil=document.createElement("div");
-   sil.style.position="fixed";
-   sil.textContent="sil";
-   sil.classList.add('sil');
-   let li=document.createElement('li');
-   li.textContent=sayac+"."+girisform.value;
-   ul.style.fontsize="2em";
-   li.style.height="1.5em";
-   
-   
-
-   	/* eklenilen satır silme butonunuda satırın yanında tanımlanması için satırın içine eklenmesi gerekiyor aşağıda ekledim*/
-     
-   li.appendChild(sil);
-
-   ul.appendChild(li);
-   girisform.value="";
+      sayac += 1;
+      let sil = document.createElement("div");
+      sil.style.position = "fixed";
+      sil.textContent = "sil";
+      sil.classList.add('sil');
+      let li = document.createElement('li');
+      li.textContent = sayac + "." + girisform.value;
+      ul.style.fontsize = "2em";
+      li.style.height = "1.5em";
 
 
 
-   /*sil butonuna tıklanan satırdaki satırı silen kod*/
-    function secilenisil()
-    {
-    	sayac--;
-    	bildirim.textContent="Seçtiğiniz satır silindi";
-    	setTimeout(() => {bildirim.textContent=" "},3000);
-    	li.remove();
-    }
-   sil.addEventListener('click', secilenisil);
+      /* eklenilen satır silme butonunuda satırın yanında tanımlanması için satırın içine eklenmesi gerekiyor aşağıda ekledim*/
+
+      li.appendChild(sil);
+
+      ul.appendChild(li);
+      girisform.value = "";
 
 
-   
-   /* Listeye yeni bir satır eklendiğinde eklendi bildirimi gelip 2 saniye sonra silinen bildirim kodu*/ 
-    bildirim.textContent="Eklendi";
-    setTimeout(() => {bildirim.textContent=" "},3000);
 
- 
+      /*sil butonuna tıklanan satırdaki satırı silen kod*/
+      function secilenisil() {
+
+         sayac--;
+         bildirim.textContent = "Seçtiğiniz satır silindi";
+         setTimeout(() => { bildirim.textContent = " " }, 3000);
+         li.remove();
+      }
+      sil.addEventListener('click', secilenisil);
+
+
+
+      /* Listeye yeni bir satır eklendiğinde eklendi bildirimi gelip 2 saniye sonra silinen bildirim kodu*/
+      bildirim.textContent = "Eklendi";
+      setTimeout(() => { bildirim.textContent = " " }, 3000);
+
+
 
 
 
@@ -60,16 +58,15 @@ function ekle()
 }
 
 
-function remove()
-{
- sayac--;
-	ul.textContent="";
-	bildirim.textContent="Liste temizlendi";
-	setTimeout(() => {bildirim.textContent=" "},3000);
+function remove() {
+   sayac--;
+   ul.textContent = "";
+   bildirim.textContent = "Liste temizlendi";
+   setTimeout(() => { bildirim.textContent = " " }, 3000);
 }
 
 
 
 
-temizle.addEventListener('click',remove);
-buton.addEventListener('click',ekle);
+temizle.addEventListener('click', remove);
+buton.addEventListener('click', ekle);
